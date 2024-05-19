f2553e8d0e234d962b6d6eb68875b5aea24dff5c
abushiyyab <alis@simplify9.com>
2024-05-19 12:30:41 +0300

    fully responsive for the home page

--- src/components/common/footer/join-mail-list-section.tsx
+++ src/components/common/footer/join-mail-list-section.tsx
@@ -3,10 +3,10 @@ import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
 
 export const JoinMailListSection = () => {
     return (
-        <div className={'w-full flex item-start justify-between'}>
+        <div className={'w-full flex flex-col lg:flex-row item-start justify-between gap-4 md:gap-0'}>
             <div className="flex flex-col md:flex-row justify-center">
-                <form className="flex items-center space-x-12">
-                    <div className={"flex flex-row gap-2"}>
+                <form className="flex flex-wrap md:flex-row gap-4 items-starts md:items-center md:space-x-12">
+                    <div className={"flex flex-row gap-2 items-center"}>
                         <input type="checkbox" className="accent-gray-700"/>
                         <span>Join Our Mailing List</span>
                     </div>
@@ -22,10 +22,12 @@ export const JoinMailListSection = () => {
                                placeholder={"Enter your email here"}
                                className="border border-gray-300 p-2 rounded text-gray-700"/>
                     </div>
-                    <button className="text-white px-4 py-2 rounded bg-red-800">Subscribe</button>
+                    <div className={"flex flex-col items-center"}>
+                    <button className="text-white w-fit px-4 py-2 rounded bg-red-800">Subscribe</button>
+                    </div>
                 </form>
             </div>
-            <div className="flex justify-end items-center gap-8">
+            <div className="flex justify-center md:justify-end items-center gap-8">
                 <a href="#" className="text-blue-700 hover:text-gray-900"><FaFacebook size={28}/></a>
                 <a href="#" className="text-red-700 hover:text-gray-900"><FaInstagram size={28}/></a>
                 <a href="#" className="text-blue-700 hover:text-gray-900"><FaTwitter size={28}/></a>
