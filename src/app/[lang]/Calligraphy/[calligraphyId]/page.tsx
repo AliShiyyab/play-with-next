5bba5260a564b973204699b975d99675f4f6afd5
abushiyyab <alis@simplify9.com>
2024-05-20 12:57:23 +0300

    fix

--- src/app/[lang]/Calligraphy/[calligraphyId]/page.tsx
+++ src/app/[lang]/Calligraphy/[calligraphyId]/page.tsx
@@ -1,7 +1,11 @@
+import {Contact} from "@/components/shared/contact";
+
 const Page = ({params}: {params: {lang: string; calligraphyId: string}}) => {
     console.log(params);
     
-    return null;
+    return (
+        <Contact title={"Contact Calligrapher"} />
+    );
 }
 
 export default Page;
--- src/components/shared/contact.tsx
+++ src/components/shared/contact.tsx
@@ -10,7 +10,7 @@ export const Contact = (props: IContact) => {
     const {title} = props;
     const [contactDetails, setContactDetails] = useState({name: "", email: "", message: ""})
     const handleSendEmail = () => {
-    // install yarn add @sendgrid/mail then do the other process (reference : https://medium.com/@toralf.frich/simple-email-delivery-from-next-js-with-sendgrid-b12ce04cffe3)
+        // install yarn add @sendgrid/mail then do the other process (reference : https://medium.com/@toralf.frich/simple-email-delivery-from-next-js-with-sendgrid-b12ce04cffe3)
     }
 
     const handleChange = (e: any) => {
@@ -18,10 +18,10 @@ export const Contact = (props: IContact) => {
     }
 
     return (
-        <div className={'flex flex-row px-4 md:px-12'}>
-            <div className={'flex flex-col p-2 md:p-4 lg:p-6 border rounded-2xl shadow-xl'}>
-                <h1 className={"text-center text-3xl md:text-4xl mb-5 text-red-900 font-bold"}>{title}</h1>
-                <div className={"flex flex-col md:flex-row justify-between"}>
+        <div className={'flex flex-row px-4 md:p-12 w-full text-red-900'}>
+            <div className={'flex flex-col w-full p-2 md:p-4 lg:p-6 border rounded-2xl shadow-xl'}>
+                <h1 className={"text-start text-xl md:text-2xl mb-5 font-bold"}>{title}</h1>
+                <div className={"flex gap-4 flex-col md:flex-row justify-between"}>
                     <div className={'flex flex-col gap-4'}>
 
                         <div className={"flex flex-col md:flex-row gap-2 items-center"}>
@@ -44,27 +44,30 @@ export const Contact = (props: IContact) => {
                                    className="border border-gray-300 p-2 rounded text-gray-700"/>
                         </div>
                     </div>
-                    <div className={'flex flex-col gap-4'}>
+                    <div className={'flex flex-col md:flex-row gap-4'}>
 
-                        <div className={"flex flex-col md:flex-row gap-2 items-center"}>
-                            <span>Message</span>
+                        <div className={"flex flex-col md:flex-row gap-2 items-start"}>
+                            <span className={'pt-2'}>Message</span>
                             <textarea
                                 name={"message"}
                                 value={contactDetails.message}
                                 onChange={handleChange}
                                 placeholder={"Enter your message here"}
-                                className="border border-gray-300 p-2 rounded text-gray-700"
+                                className="border border-gray-300 p-2 rounded text-gray-700 h-full"
                             />
                         </div>
 
                     </div>
+                    <div className={"flex justify-center"}>
+                        <div className={"flex items-center"}>
+                            <button
+                                className={"px-4 py-4 border rounded-2xl bg-red-800 text-white hover:bg-white hover:text-red-800 "}
+                                onClick={handleSendEmail}>Send
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className={"flex justify-center"}>
-                    <button
-                        className={"px-4 py-2 border rounded-2xl bg-red-800 text-white hover:bg-white hover:text-red-800"}
-                        onClick={handleSendEmail}>Send
-                    </button>
-                </div>
+
             </div>
         </div>
     )
